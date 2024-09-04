7040a11d77c22b8b0f2cb8409f1a2b277f2f4eda
kody <w.jamesdevera@gmail.com>
2024-09-04 21:13:17 +0800

    updated configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,8 +2,4 @@ const express = require("express");
 
 const app = express();
 
-app.get("/hello", (request, response) => {
-  response.status(200).json({ message: "Hello World" });
-});
-
 module.exports = app;
--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + "/../../.env" });
 
 const config = {
   port: process.env.PORT || 8000,
